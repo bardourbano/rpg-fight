2a347650294a57de58a8e09fb484fde906c4180a
João Melo <joaomelo.contato@outlook.com>
2021-03-27 16:20:25 -0300

    criando as bases da aplicação

---
+++ app/Http/Controllers/BatalhaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Batalha;
+use Illuminate\Http\Request;
+
+class BatalhaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Batalha  $batalha
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Batalha $batalha)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Batalha  $batalha
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Batalha $batalha)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Batalha  $batalha
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Batalha $batalha)
+    {
+        //
+    }
+}
